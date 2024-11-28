0a748a19d4be0051dafde0fe89a014330ad75862
Rigmanyi-Lorand <125259951+Rigmanyi-Lorand@users.noreply.github.com>
2024-11-28 13:02:00 +0200

    Add API endpoint to list user-owned cars

--- src/api/repositories/repository.py
+++ src/api/repositories/repository.py
@@ -1,20 +1,21 @@
 from sqlalchemy.orm import Session
 from db.tables.models import Car, Dealer, SoldCar
 from datetime import datetime
+import re
 
 def add_car(db: Session, car_data: dict):
     required_fields = ['model', 'km', 'year', 'price', 'combustible', 'gearbox', 'body_type', 'cylinder_capacity', 'power', 'dateof_post', 'id_post', 'img_url']
     for field in required_fields:
         if field not in car_data:
             raise ValueError(f"Missing required field: {field}")
-
+        
     try:
-        car_data['km'] = float(car_data['km'].replace(" km", "").replace(" ", ""))
-        car_data['price'] = float(car_data['price'].replace(" ", "").replace(",", "."))
+        car_data['km'] = int(re.sub(r'[a-zA-Z ]', '', car_data['km']))
+        car_data['price'] = float(re.sub(r'[a-zA-Z ]', '', car_data['price']).replace(",", "."))
         car_data['year'] = int(car_data['year'])
-        car_data['power'] = int(car_data['power'].replace(" CP", "").replace(" ", ""))
+        car_data['power'] = int(re.sub(r'[a-zA-Z ]', '', car_data['power']))
         if car_data.get('cylinder_capacity') is not None:
-            car_data['cylinder_capacity'] = int(car_data['cylinder_capacity'].replace(" cm3", "").replace(" ", ""))
+            car_data['cylinder_capacity'] = int(re.sub(r'[a-zA-Z ]', '', car_data['cylinder_capacity']))
         else:
             car_data['cylinder_capacity'] = 0 
         car_data['dateof_post'] = datetime.strptime(car_data['dateof_post'], '%d.%m.%Y').date()
